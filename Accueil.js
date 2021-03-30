58be574ea01a7a11fddc19b1221756aaf237c6e0
Adib610 <adib.ben1234@gmail.com>
2021-03-30 12:21:56 -0400

    Dernière version du site

---
+++ Accueil.js
@@ -0,0 +1,187 @@
+var fenetres = document.getElementsByTagName('span');
+
+function FermerFenetres(){
+    for (let index = 1; index < fenetres.length; index++) {
+        document.querySelector('#Fenetre' + index).style.display = "none";
+    }
+}
+
+document.querySelector('#Detail0').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.getElementById('Fenetre0').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail1').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+    
+    document.getElementById('Fenetre1').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail2').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+
+    document.getElementById('Fenetre2').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail3').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+
+    document.getElementById('Fenetre3').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail4').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+
+    document.getElementById('Fenetre4').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail5').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+
+    document.getElementById('Fenetre5').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail6').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+
+    document.getElementById('Fenetre6').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail7').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+
+    document.getElementById('Fenetre7').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail8').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+
+    document.getElementById('Fenetre8').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('#Detail9').addEventListener('click', 
+function(){
+    FermerFenetres();
+    document.querySelector('#Fenetre0').style.display = "none";
+    document.querySelector('#Fenetre1').style.display = "none";
+    document.querySelector('#Fenetre2').style.display = "none";
+    document.querySelector('#Fenetre3').style.display = "none";
+    document.querySelector('#Fenetre4').style.display = "none";
+    document.querySelector('#Fenetre5').style.display = "none";
+    document.querySelector('#Fenetre6').style.display = "none";
+    document.querySelector('#Fenetre7').style.display = "none";
+    document.querySelector('#Fenetre8').style.display = "none";
+    document.querySelector('#Fenetre9').style.display = "none";
+
+    document.getElementById('Fenetre9').style.display = 'block';
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('.bg-model').addEventListener('click', 
+function(){
+    document.querySelector('.bg-model').style.display = 'none';
+});
+
+
+// (function(){
+//     document.querySelector('.bg-model').style.display = 'flex';
+//     print('hello');
+// });
